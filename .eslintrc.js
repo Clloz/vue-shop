83b2d2a95aaca4953a8d8803281c28ddfc54cd7e
Clloz <clloz@outlook.com>
2020-11-24 15:44:59 +0800

    eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,54 @@
+/*
+ * @Author: Clloz
+ * @Date: 2020-11-24 13:32:09
+ * @LastEditTime: 2020-11-24 13:46:29
+ * @LastEditors: Clloz
+ * @Description:
+ * @FilePath: /vue-shop/.eslintrc.js
+ * @博观而约取，厚积而薄发，日拱一卒，日进一寸。
+ */
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    plugins: ['prettier', 'html'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'prettier/prettier': [
+            'warn',
+            {
+                printWidth: 100,
+                tabWidth: 4,
+                useTabs: false,
+                semi: true,
+                singleQuote: true,
+                jsxSingleQuote: false,
+                trailingComma: 'all',
+                quoteProps: 'as-needed',
+                bracketSpacing: true,
+                jsxBracketSameLine: false,
+                arrowParens: 'avoid',
+                requirePragma: false,
+                insertPragma: false,
+                proseWrap: 'preserve',
+                htmlWhitespaceSensitivity: 'ignore',
+                vueIndentScriptAndStyle: false,
+                endOfLine: 'auto',
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: Clloz
+ * @Date: 2020-11-24 13:43:14
+ * @LastEditTime: 2020-11-24 13:43:39
+ * @LastEditors: Clloz
+ * @Description:
+ * @FilePath: /vue-shop/.prettierrc.js
+ * @博观而约取，厚积而薄发，日拱一卒，日进一寸。
+ */
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'ignore',
+    vueIndentScriptAndStyle: false,
+    endOfLine: 'auto',
+    // embeddedLanguageFormatting: 'off',
+};
